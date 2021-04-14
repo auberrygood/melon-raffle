ff068278f1c89f843f6243c0e70173a759b1b6f6
Aubrey <aubrey.sandrs@gmail.com>
2021-04-13 17:44:29 -0700

    updated version 2 raffle.py to import functions from customers.py; copy/pasted main functions from version 1 raffle.py to version 2 raffle.py

--- version2/raffle.py
+++ version2/raffle.py
@@ -1,6 +1,31 @@
 """Randomly pick customer and print customer info"""
 
 # Add code starting here
+import random
 
 # Hint: remember to import any functions you need from
 # other files or libraries
+from customers import get_customers_from_file
+
+
+
+def pick_winner(customers):
+    """Choose a random winner from list of customers."""
+
+    chosen_customer = random.choice(customers)
+    
+    name = chosen_customer.name
+    email = chosen_customer.email
+
+    print(f"Tell {name} at {email} that they've won")
+
+
+def run_raffle():
+    """Run the weekly raffle."""
+
+    customers = get_customers_from_file("customers.txt")
+    pick_winner(customers)
+
+
+if __name__ == "__main__":       # only do the following if the program is run directly (i.e. python3 raffle.py versus running some other program with run_raffle() being an imported function)
+    run_raffle()                 # read customers.txt, organize data and store customers in customer list, then pick winner  
